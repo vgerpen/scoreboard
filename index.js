06213421c0b14c06c73ac7727cda9bda6c84db41
vgerpen <39973464+vgerpen@users.noreply.github.com>
2022-08-21 11:04:16 -0700

    Changed a word

---
+++ index.js
@@ -0,0 +1,32 @@
+let StartinghomeScore = 0
+document.getElementById("home-score").innerText = StartinghomeScore;
+
+
+let StartingguestScore = 0
+
+function addOneleft(){
+StartinghomeScore += 1;
+document.getElementById("home-score").innerText = StartinghomeScore;
+}
+
+function addTwoleft(){
+StartinghomeScore += 2;
+document.getElementById("home-score").innerText = StartinghomeScore;    
+}
+
+function addThreeleft(){
+StartinghomeScore += 3;
+document.getElementById("home-score").innerText = StartinghomeScore;    
+}
+
+function addOneright(){
+    
+}
+
+function addTworight(){
+    
+}
+
+function addThreeright(){
+    
+}
